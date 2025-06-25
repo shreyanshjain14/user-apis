cc5b47ed88ff1d38196df637fe57cf5347404c48
Shreyansh-Jain14 <shreyansh.jain@squareboat.com>
2025-06-25 14:16:22 +0530

    baseline setup

--- apps/user-apis/src/controllers/admin.ts
+++ apps/user-apis/src/controllers/admin.ts
@@ -1,31 +1,22 @@
+import { UserTransformer } from "@libs/common";
 import { Request, Response, RestController } from "@libs/core";
 import { Dto, Validate } from "@libs/core/validator";
-import { UserTransformer } from "@libs/common";
-import {
-  Controller,
-  Get,
-  Put,
-  Req,
-  Res,
-  UseGuards,
-} from "@nestjs/common";
+import { Controller, Get, Put, Req, Res, UseGuards } from "@nestjs/common";
 
-import { AssignRoleDto, ListUsersDto } from "../dtos";
 import { Roles } from "@libs/users/decorators";
-import { UserApiService } from "../services/user";
 import { JwtGuard, RolesGuard } from "@libs/users/guards";
+import { AssignRoleDto, ListUsersDto } from "../dtos";
+import { UserApiService } from "../services/user";
 
 @Controller("admin")
 @UseGuards(JwtGuard, RolesGuard)
 export class AdminAuthController extends RestController {
-  constructor(
-    private readonly userService: UserApiService
-  ) {
+  constructor(private readonly userService: UserApiService) {
     super();
   }
 
-  @Get('users')
-  @Roles('admin')
+  @Get("users")
+  @Roles("admin")
   @Validate(ListUsersDto)
   async getUsers(
     @Dto() dto: ListUsersDto,
@@ -36,19 +27,23 @@ export class AdminAuthController extends RestController {
     return res.withMeta(
       await this.paginate(users, new UserTransformer(), {
         req,
-      }),
+      })
     );
   }
 
   @Put("users/:id")
   @Roles("admin")
   @Validate(AssignRoleDto)
-  async assignRole(@Dto() inputs: AssignRoleDto, @Req() req: Request, @Res() res: Response) {
+  async assignRole(
+    @Dto() inputs: AssignRoleDto,
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
     const data = await this.userService.assignRole(inputs);
     return res.success(
       await this.transform(data, new UserTransformer(), {
         req,
-      }),
+      })
     );
   }
 }
